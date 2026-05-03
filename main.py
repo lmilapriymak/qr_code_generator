deaeafd60e401da758f1ccd28f5fce5a80e205dc
ludmila <ludmila@whatever.com>
2026-05-03 22:02:47 +0300

    Add web form with default message

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ from io import BytesIO
 
 app = Flask(__name__)
 
+@app.route("/")
+def root():
+    return '''<form action="/qr" method="GET">
+                <input type="text" name="msg" placeholder="Enter text"/>
+                <input type="submit" value="QR-code"/>
+               </form>'''
+
 @app.route("/qr")
 def qr():
-   msg = request.args.get('msg')
-   img = qrcode.make(msg)
-   
-   buffer = BytesIO()
-   img.save(buffer, format="png")
- 
-   img64 = base64.b64encode(buffer.getvalue())
-   return f'<img src="data:image/png;base64, {img64.decode()}" alt="qrcode" />'
-
+    msg = request.args.get('msg')
+    if not msg:
+        msg = "Hello from Jenkins!"
+    img = qrcode.make(msg)
+    buffer = BytesIO()
+    img.save(buffer, format="png")
+    img64 = base64.b64encode(buffer.getvalue())
+    return f'<img src="data:image/png;base64, {img64.decode()}" alt="qrcode" />'
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
